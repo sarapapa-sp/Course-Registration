65e815699db32124ace9ec9545395706c22ebb17
sarapapa-sp <shubhampawar1705@gmail.com>
2022-09-10 15:57:41 +0530

    added connectivity to sqlite database to add the record

--- app/src/main/java/com/example/coursereg/MainActivity.java
+++ app/src/main/java/com/example/coursereg/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.coursereg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 //    Creating variables
@@ -35,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         btnok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                insert();
             }
         });
     }
@@ -46,9 +50,20 @@ public class MainActivity extends AppCompatActivity {
             nameData=name.getText().toString();
             String courseData = course.getText().toString();
             String feesData = fees.getText().toString();
+//            TODO : Creating and accessing the SQLite Database and adding data to the database
+            SQLiteDatabase db = openOrCreateDatabase("SQLite", Context.MODE_PRIVATE,null);
+            db.execSQL("create table if not exists records(id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR,course VARCHAR,fee VARCHAR )");
+            String sql = "insert into records(name,course,fee) values (?,?,?)";
+            SQLiteStatement statement=db.compileStatement(sql);
+            statement.bindString(1,nameData);
+            statement.bindString(2,courseData);
+            statement.bindString(3,feesData);
+            statement.execute();
 
+
+            Toast.makeText(this,"Record was added successfully",Toast.LENGTH_LONG).show();
         }catch (Exception ex){
-
+            Toast.makeText(this,"Record was not added",Toast.LENGTH_LONG).show();
         }
     }
 }
