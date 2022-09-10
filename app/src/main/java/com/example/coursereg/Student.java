60b99d4ccd4d28932bb73a9e96c03e3368a19ca6
sarapapa-sp <shubhampawar1705@gmail.com>
2022-09-10 18:11:46 +0530

    added backend to display the list by rawquery to display the data

---
+++ app/src/main/java/com/example/coursereg/Student.java
@@ -0,0 +1,10 @@
+package com.example.coursereg;
+
+public class Student {
+//    Creating the variables to access
+    String id;
+    String name;
+    String course;
+    String fee;
+    String title;
+}
---
+++ app/src/main/java/com/example/coursereg/view.java
@@ -0,0 +1,61 @@
+package com.example.coursereg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class view extends AppCompatActivity {
+    /*
+    * Variables to store the data and the list view
+    * */
+    ListView list;
+    ArrayList<String> titleData = new ArrayList<>();
+    ArrayAdapter arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+//        Connecting with the DB
+        SQLiteDatabase db = openOrCreateDatabase("SQLite", Context.MODE_PRIVATE,null);
+
+        list=findViewById(R.id.list);
+        final Cursor cursor=db.rawQuery("select * from records",null);
+        int id=cursor.getColumnIndex("1");
+        int name=cursor.getColumnIndex("name");
+        int course=cursor.getColumnIndex("course");
+        int fee=cursor.getColumnIndex("fee");
+        titleData.clear();
+
+        arrayAdapter=new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,titleData);
+//      Using the Student class to store the properties
+        final ArrayList<Student> students = new ArrayList<Student>();
+
+        if(cursor.moveToFirst()){
+            do{
+                Student stud = new Student();
+                stud.id=cursor.getString(id);
+                stud.course=cursor.getString(course);
+                stud.name=cursor.getString(name);
+                stud.fee=cursor.getString(fee);
+
+                students.add(stud);
+
+                titleData.add(cursor.getString(id)+" \t "+cursor.getString(name)+ " \t "+cursor.getString(course)+ " \t "+cursor.getString(fee));
+
+            }while (cursor.moveToNext());
+
+            arrayAdapter.notifyDataSetChanged();
+            list.invalidateViews();
+        }
+
+    }
+}
